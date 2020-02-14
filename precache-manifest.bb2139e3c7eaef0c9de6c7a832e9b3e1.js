f07afa743699f406ee010ead76d4e58ad9df6f31
lavanya <nimushakavilavanya@gmail.com>
2020-02-14 15:00:59 -0800

    Updates

---
+++ precache-manifest.bb2139e3c7eaef0c9de6c7a832e9b3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb0aa71113896248ace58fe43532356",
+    "url": "/staffportal/index.html"
+  },
+  {
+    "revision": "5b71fc80a7f38bbf6b59",
+    "url": "/staffportal/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6fa9b68682ee1d3e51bb",
+    "url": "/staffportal/static/js/2.481b8c0f.chunk.js"
+  },
+  {
+    "revision": "56c465897e122494796dcef1fe489103",
+    "url": "/staffportal/static/js/2.481b8c0f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b71fc80a7f38bbf6b59",
+    "url": "/staffportal/static/js/main.e8265110.chunk.js"
+  },
+  {
+    "revision": "05e322b97d0ac60f337a",
+    "url": "/staffportal/static/js/runtime-main.e3243860.js"
+  }
+]);
